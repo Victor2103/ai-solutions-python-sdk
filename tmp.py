d6ffbb1355add227d41fdb4c7dcaf84209a352f8
Victor <vitcheffvi@cy-tech.fr>
2023-08-10 15:20:46 +0200

    test notification telegram

--- tmp.py
+++ tmp.py
@@ -1,7 +1,13 @@
-from prefect.blocks.notifications import TelegramWebhook
-from pydantic import SecretStr
+import os
 
-telegram_webhook_block = TelegramWebhook(
-    bot_token=SecretStr("0123456789:qwertyuiopasdfghjklzxcvbnm"), chat_id="123456789"
+from dotenv import load_dotenv
+from prefect.blocks.notifications import AppriseNotificationBlock
+
+# Load environments variables
+load_dotenv()
+
+telegram_webhook_block = AppriseNotificationBlock(
+    url=f"tgram://{os.getenv('API_TELEGRAM')}/{os.getenv('CHAT_ID')}/"
 )
+
 telegram_webhook_block.notify("Hello from Prefect!")
