2043f517302320c87ed3b57ad5dea576a7595d21
Victor <vitcheffvi@cy-tech.fr>
2023-08-10 14:54:45 +0200

    env var for bearer token"

--- prefect_test.py
+++ prefect_test.py
@@ -1,9 +1,14 @@
+import os
+
+from dotenv import load_dotenv
 from prefect_ovh.flows import create_job_and_wait_until_is_done
 
-TOKEN = "SAnNu2i6R+K6JOYaAUDclnTWx1XH3Ck7+hIfr2dWj4oLEbt+XvhMnvUUegm0QFY9"
+# Load environments variables
+load_dotenv()
 
-print("Here is your job : ",
-      create_job_and_wait_until_is_done(token=TOKEN,
-                                        image="bash",
-                                        command=["sleep", "120"],
-                                        timeout=30))
+TOKEN = os.getenv("TOKEN")
+
+flow = create_job_and_wait_until_is_done(
+    token=TOKEN, image="bash", command=["sleep", "120"]
+)
+print("Here is your job : ", flow)
--- tmp.py
+++ tmp.py
@@ -1,18 +1,7 @@
-from ov_hcloud_ai_solution_client import AuthenticatedClient
-from ov_hcloud_ai_solution_client.models import Me
-from ov_hcloud_ai_solution_client.api.me import me
+from prefect.blocks.notifications import TelegramWebhook
+from pydantic import SecretStr
 
-client = AuthenticatedClient(base_url="https://gra.training.ai.cloud.ovh.net",
-                             token='SAnNu2i6R+K6JOYaAUDclnTWx1XH3Ck7+hIfr2dWj4oLEbt+XvhMnvUUegm0QFY9')
-with client as client:
-    response: Me = me.sync_detailed(client=client)
-
-print(response)
-
-client = AuthenticatedClient(base_url="https://gra.training.ai.cloud.ovh.net",
-                             token='SAnNu2i6R+K6JOY7+hIfr2dWj4oLEbt+XvhMnvUUegm0QFY9')
-
-with client as client:
-    response: Me = me.sync_detailed(client=client)
-
-print(response)
+telegram_webhook_block = TelegramWebhook(
+    bot_token=SecretStr("0123456789:qwertyuiopasdfghjklzxcvbnm"), chat_id="123456789"
+)
+telegram_webhook_block.notify("Hello from Prefect!")
