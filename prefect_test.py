7150db2e0bc6c31208e9793249fde0e6ce67f417
Victor <vitcheffvi@cy-tech.fr>
2023-08-10 15:45:27 +0200

    add telegram bot

--- prefect_test.py
+++ prefect_test.py
@@ -9,6 +9,11 @@ load_dotenv()
 TOKEN = os.getenv("TOKEN")
 
 flow = create_job_and_wait_until_is_done(
-    token=TOKEN, image="bash", command=["sleep", "120"]
+    token=TOKEN,
+    image="bash",
+    command=["sleep", "120"],
+    telegram=True,
+    api_telegram=os.getenv("API_TELEGRAM"),
+    chat_id=os.getenv("CHAT_ID"),
 )
 print("Here is your job : ", flow)
